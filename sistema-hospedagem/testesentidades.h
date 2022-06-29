bd4d08d16bdb7eebf1b31721668c2a5510f40000
marcelo amorim <marcelo.med.amorim@gmail.com>
2022-06-28 21:08:24 -0300

    adiciona testes de dominios e entidades

--- sistema-hospedagem/testesentidades.h
+++ sistema-hospedagem/testesentidades.h
@@ -1,42 +1,10 @@
-// Diretivas de compilação para controlar processo de inclusão.
-
-#ifndef TESTES_H_INCLUDED
-#define TESTES_H_INCLUDED
-
-// Incluir cabeçalhos.
-
-#include "dominios.h"
-#include "entidades.h"
-
-using namespace std;
-
 // --------------------------------------------------------------------------
-// Exemplo de declaração de classe para teste de unidade de classe domínio.
+// Declaração de classe para teste de unidade de classe entidade = Usuario.
 
-class TUCodigo {
+class TUUsuario {
 private:
     const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
-    const static int VALOR_INVALIDO = 30;   // Definição de constante para evitar número mágico.
-    Codigo *codigo;                         // Referência para unidade em teste.
-    int estado;                             // Estado do teste.
-    void setUp();                           // Método para criar unidade em teste.
-    void tearDown();                        // Método para destruir unidade em teste.
-    void testarCenarioSucesso();            // Cenário de teste.
-    void testarCenarioFalha();              // Cenário de teste.
-
-public:
-    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
-    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
-    int run();                              // Método para executar teste.
-};
-
-// --------------------------------------------------------------------------
-// Exemplo de declaração de classe para teste de unidade de classe entidade.
-
-class TUProjeto {
-private:
-    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
-    Projeto *projeto;                       // Referência para unidade em teste.
+    Usuario *usuario;                       // Referência para unidade em teste.
     int estado;                             // Estado do teste.
     void setUp();                           // Método para criar unidade em teste.
     void tearDown();                        // Método para destruir unidade em teste.
@@ -47,4 +15,39 @@ public:
     int run();                              // Método para executar teste.
 };
 
-#endif // TESTES_H_INCLUDED
+// --------------------------------------------------------------------------
+// Declaração de classe para teste de unidade de classe entidade = Hospedagem.
+
+class TUHospedagem {
+private:
+    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
+    Hospedagem *hospedagem;                       // Referência para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // Método para criar unidade em teste.
+    void tearDown();                        // Método para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cenário de teste.
+public:
+    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
+    int run();                              // Método para executar teste.
+};
+
+
+// --------------------------------------------------------------------------
+// Declaração de classe para teste de unidade de classe entidade = Avaliacao.
+
+class TUAvaliacao {
+private:
+    const static int VALOR_VALIDO   = 20;   // Definição de constante para evitar número mágico.
+    Avaliacao *avaliacao;                       // Referência para unidade em teste.
+    int estado;                             // Estado do teste.
+    void setUp();                           // Método para criar unidade em teste.
+    void tearDown();                        // Método para destruir unidade em teste.
+    void testarCenarioSucesso();            // Cenário de teste.
+public:
+    const static int SUCESSO =  0;          // Definição de constante para reportar resultado de teste.
+    const static int FALHA   = -1;          // Definição de constante para reportar resultado de teste.
+    int run();                              // Método para executar teste.
+};
+
+
